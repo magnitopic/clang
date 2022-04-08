61bfc0192243c2346c39d00fb6c45d55265953c6
magnitopic <magnitopic@gmail.com>
2022-04-08 14:12:33 +0200

    arrays

---
+++ arrays.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int luckyNumbers[3];
+	luckyNumbers[2] = 3;
+	printf("%d\n", luckyNumbers[2]);
+	luckyNumbers[1] = 2;
+	printf("%d\n", luckyNumbers[1]);
+	luckyNumbers[0] = 1;
+	printf("%d\n", luckyNumbers[0]);
+	return 0;
+}
