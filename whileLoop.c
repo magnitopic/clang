cf7f3623430996cf9cf9d79779f95d710f56b9ef
magnitopic <magnitopic@gmail.com>
2022-04-09 19:11:20 +0200

    While loop

---
+++ whileLoop.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int index = 1;
+	while (index <= 7)
+	{
+		printf("%d\n", index);
+		index++;
+	}
+
+	printf("______________");
+
+	index = 1;
+	do
+	{
+		printf("%d\n", index);
+		index++;
+	} while (index <= 5);
+	return 0;
+}
