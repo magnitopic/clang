b7fe8d40e2dcc381e2a73eb02d13eeede027766a
magnitopic <magnitopic@gmail.com>
2022-04-10 20:19:33 +0200

    2D Arrays

---
+++ 2dArrays.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int nums[3][2] = {{1, 2}, {3, 4}, {5, 6}};
+	printf("%d\n\n", nums[1][1]);
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			printf("%d, ", nums[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
