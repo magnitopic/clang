f4154ca8c633dea20f009c3e5bdc4ff20069f1f0
magnitopic <magnitopic@gmail.com>
2022-04-10 20:47:52 +0200

    writing to a file

---
+++ writingFiles.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	/* Creating and writing the file */
+	FILE *fpointer = fopen("test.txt", "w");
+
+	fprintf(fpointer, "Hello\nThere\n");
+
+	fclose(fpointer);
+
+	/* Appending more text */
+	FILE *fpointer2 = fopen("test.txt", "a");
+
+	fprintf(fpointer2, "\nGeneral\nKenobi");
+
+	fclose(fpointer2);
+	return 0;
+}
