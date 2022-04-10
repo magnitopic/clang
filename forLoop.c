6b26a2f93dd8c88fa4bc790c364bbf34a6a06dcf
magnitopic <magnitopic@gmail.com>
2022-04-10 20:07:21 +0200

    For loop over an array

--- forLoop.c
+++ forLoop.c
@@ -3,9 +3,10 @@
 
 int main()
 {
-	for (int i = 1; i <= 5; i++)
+	int luckyNumbers[] = {2, 4, 8, 16, 32, 64, 128, 256};
+	for (int i = 0; i < sizeof(luckyNumbers) / sizeof(luckyNumbers[i]); i++)
 	{
-		printf("%d\n", i);
+		printf("%d\n", luckyNumbers[i]);
 	}
 	return 0;
 }
