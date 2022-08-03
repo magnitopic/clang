2d0cfee5b7850fb71b06a6c5f15ee3d9174d7658
magnitopic <magnitopic@gmail.com>
2022-08-03 13:06:34 +0200

    small madLibGame change

--- madLibGame.c
+++ madLibGame.c
@@ -4,17 +4,17 @@ int main(void)
 {
 	char color[20];
 	char noun[20];
-	char celebrety[20];
+	char lovedone[20];
 
 	printf("Enter a color: ");
 	scanf("%s", color);
 	printf("Enter a plural noun: ");
 	scanf("%s", noun);
-	printf("Enter a celebrety's name: ");
-	scanf("%s", celebrety);
+	printf("Enter a loved one's name: ");
+	scanf("%s", lovedone);
 
 	printf("\nRoses are %s\n", color);
 	printf("%s are blue\n", noun);
-	printf("I love %s\n", celebrety);
+	printf("I love %s\n", lovedone);
 	return 0;
 }
