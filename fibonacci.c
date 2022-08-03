4ea73c6f88f35b233e5e0906a8eff39be8d7ee31
magnitopic <magnitopic@gmail.com>
2022-08-03 17:16:55 +0200

    fibonacci

--- fibonacci.c
+++ fibonacci.c
@@ -4,12 +4,11 @@
 int	main(void)
 {
 	int a = 0, b = 1;
-	for (int i = 1; i <= 42; i++)
+	for (int i = 0; i <= 42; i++)
 	{
 		printf("%d: %d\n", i, a);
 		a = a + b;
 		b = a - b;
 	}
-
 	return 0;
 }
---
+++ recursion/fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int c = 1;
+
+int	fibonacci(int num)
+{
+	if (num <= 0)
+		return (0);
+	if (num == 1)
+		return (1);
+	return (fibonacci(num - 1) + fibonacci(num - 2));
+}
+
+int	main(void)
+{
+	for (int i = 42; i >= 0; i--)
+		printf("%d: %d\n", i, fibonacci(i));
+	return (0);
+}
