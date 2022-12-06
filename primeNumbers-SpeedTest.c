31add450858b7210cfd81cdd685f3abb65163a91
magnitopic <magnitopic@gmail.com>
2022-12-06 23:24:13 +0100

    program for cpu benchmarking

---
+++ primeNumbers-SpeedTest.c
@@ -0,0 +1,32 @@
+// complie with: gcc primeNumbers-SpeedTest.c -lm
+// MacBookAir M2 2022 with 10000000 - 1.273690 seconds
+
+#include <math.h>
+#include <stdio.h>
+#include <time.h>
+int main(){
+    int n,i,j,isPrime;
+    clock_t begin = clock();
+    n=10000000;
+    printf("Prime Numbers are: \n");
+    printf("%d ",2);    // All prime numbers are odd except two
+    for(i=3; i<=n; i+=2)
+    {
+        isPrime = 1; // boolean. By default a number is prime
+        for(j=3; j<= sqrt(i); j+=2)
+        {
+            if(i%j==0)
+            {
+                isPrime=0;  // isPrime=False
+                break;
+            }
+        }
+        if(isPrime)
+            printf("%d " ,i);
+    }
+
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("\nElapsed: %f seconds\n", time_spent);
+    return 0;
+}
